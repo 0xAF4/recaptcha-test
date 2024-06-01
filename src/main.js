a6349a002b23fd7ec7267db2555134340d41841c
0xAF4 <karim.badalov1612@gmail.com>
2024-06-01 12:56:05 +0500

    recaptcha-test

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
+import { VueRecaptchaPlugin } from 'vue-recaptcha'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(VueRecaptchaPlugin, {
+  v2SiteKey: '6LdJwe0pAAAAAGxNiJVbTdIxQZ0vdsHGBlMOWTzA',
+})
+
+app.mount('#app')
